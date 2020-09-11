b73eb042b48c461709c052fae177bd38ebeb3f7c
or mizrahi <ormizrahi777@gmail.com>
2020-09-11 12:36:44 +0300

    added materialUI module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MaterialModule } from './modules/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NouisliderModule } from 'ng2-nouislider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlightService } from './services/flight-service';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		MaterialModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserModule,
+		NouisliderModule,
+		MaterialModule,
+		BrowserAnimationsModule,
+	],
+	providers: [FlightService, DatePipe],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
